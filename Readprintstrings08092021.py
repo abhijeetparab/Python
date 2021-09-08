3c057c6b4da1066f5af07b826331e0f5de6d6e1b
unknown <abhiparabrocks@gmail.com>
2021-09-08 17:54:31 +0530

    Read and Print strings

---
+++ Readprintstrings08092021.py
@@ -0,0 +1,6 @@
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python.
+
+def print_full_name(first_name, last_name):
+    print("Hello {} {}! You just delved into python.".format(first_name, last_name))
